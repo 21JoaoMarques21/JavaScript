daf8ebc608d37b7476a0f888518061f322c8eaf2
21JoaoMarques21 <joao.mendes@le-campus-numerique.fr>
2025-01-08 15:19:55 +0100

    complet exo 4

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,15 @@
 // you can write js here
 console.log('exo-2');
+let myDate = new Date();  // Crée un nouvel objet Date avec la date et l'heure actuelles
+let jourActuel = myDate.getDay();  // Récupère le jour actuel de la semaine (0 = dimanche, 6 = samedi)
+
+let jourSemaineMessage;
+let weekendMessage = "C'est le week-end.";  // Message pour le week-end
+
+if (jourActuel >= 1 && jourActuel <= 5) {  // Si le jour est entre lundi (1) et vendredi (5)
+    jourSemaineMessage = "C'est un jour de semaine.";
+} else {  // Sinon, c'est le week-end (samedi ou dimanche)
+    jourSemaineMessage = weekendMessage;
+}
+
+console.log(jourSemaineMessage);  // Affiche le message correspondant dans la console
--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,26 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+// Initialisation du tableau secretMessage
+let secretMessage = ['Learning', 'to', 'code', 'is', 'easily', 'done'];
+
+// 1. Supprimer le dernier élément du tableau
+secretMessage.pop();
+
+// 2. Ajouter les mots 'to' et 'program' à la fin du tableau
+secretMessage.push('to', 'program');
+
+// 3. Changer le mot 'easily' en 'right' en accédant à l'index correct
+secretMessage[4] = 'right';  // L'index de 'easily' est 4
+
+// 4. Supprimer le premier élément du tableau
+secretMessage.shift();
+
+// 5. Ajouter le mot "Programming" au début du tableau
+secretMessage.unshift('Programming');
+
+// 6. Remplacer la série de cinq mots : 'get', 'right', 'the', 'first', 'time' par un seul mot : 'know'
+secretMessage.splice(2, 5, 'know'); // À partir de l'index 2, on supprime 5 éléments et on les remplace par 'know'
+
+// 7. Utiliser la méthode .join pour afficher le message secret dans la console
+console.log(secretMessage.join(' ')); // Affiche le message en une seule chaîne
